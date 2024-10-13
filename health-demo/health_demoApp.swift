109cba15796bc9e5053f63398dfa5b90dac21bb7
LuoYanghao <luoyanghao@bytedance.com>
2024-10-13 19:18:40 +0800

    Initial Commit

---
+++ health-demo/health_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  health_demoApp.swift
+//  health-demo
+//
+//  Created by ByteDance on 2024/10/13.
+//
+
+import SwiftUI
+
+@main
+struct health_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
